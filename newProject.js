7f079e8ede9e5d38a1eeccc285cafa026b6c1abf
dantiketa <59174256+dantiketa@users.noreply.github.com>
2020-03-05 21:53:09 +0100

    Finished toggle menu function

--- newProject.js
+++ newProject.js
@@ -2,10 +2,16 @@
 function menu(){
 let button = document.querySelector("#icon");
 let div = document.querySelector("#menu-options");
+let header = document.querySelector(".page-header");
+
 
 button.addEventListener("click", function(){
-  if(div.style.display == "none")  {div.style.display = "block";
-} else {div.style.display = "none"}
+  if(div.style.display == "none")  {div.style.display = "flex"; 
+  div.style.flexWrap = "wrap";
+  div.style.justifyContent = "space-around";
+  header.style.marginTop = "4.9rem";
+} else {div.style.display = "none"
+header.style.marginTop = "2.53rem"}
 })
 }
 
